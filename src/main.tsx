3e883b0e89f3125f3a9ee9145843ab18149a5efc
nheingit <60185486+nheingit@users.noreply.github.com>
2024-04-19 22:56:34 -0400

    scaffold complete

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./App.css";
+
+import { TooltipProvider } from "@/components/ui/tooltip";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
+  </React.StrictMode>,
+);
