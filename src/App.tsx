3b304854690989eae350e515bef3d4c454191183
nheingit <60185486+nheingit@users.noreply.github.com>
2024-04-20 10:24:29 -0400

    add opts to oai call

--- src/App.tsx
+++ src/App.tsx
@@ -39,10 +39,18 @@ export function Dashboard() {
   const handleSubmit = async (event: any) => {
     event.preventDefault();
     const content = event.target.elements.content.value;
+    const systemContent = event.target.elements.roleContent.value;
+    const topP = event.target.elements.top_p.value;
+    const temperature = event.target.elements.temperature.value;
 
-    const messages = [{ role: "user", content: content }];
-    console.log(messages);
-    await fetchChatResponse(messages);
+    const messages = [
+      { role: "system", content: systemContent },
+      { role: "user", content: content },
+    ];
+    await fetchChatResponse(messages, {
+      top_p: topP,
+      temperature: temperature,
+    });
   };
   return (
     <div className="grid h-screen w-full pl-[56px]">
@@ -77,98 +85,30 @@ export function Dashboard() {
       <div className="flex flex-col">
         <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4">
           <h1 className="text-xl font-semibold">Playground</h1>
-          <Drawer>
-            <DrawerTrigger asChild>
-              <Button variant="ghost" size="icon" className="md:hidden">
-                <Settings className="size-4" />
-                <span className="sr-only">Settings</span>
-              </Button>
-            </DrawerTrigger>
-            <DrawerContent className="max-h-[80vh]">
-              <DrawerHeader>
-                <DrawerTitle>Configuration</DrawerTitle>
-                <DrawerDescription>
-                  Configure the settings for the model and messages.
-                </DrawerDescription>
-              </DrawerHeader>
-              <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
+        </header>
+        <main>
+          <form
+            className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3"
+            onSubmit={handleSubmit}
+          >
+            <div
+              className="relative hidden flex-col items-start gap-8 md:flex"
+              x-chunk="dashboard-03-chunk-0"
+            >
+              <div className="grid w-full items-start gap-6">
                 <fieldset className="grid gap-6 rounded-lg border p-4">
                   <legend className="-ml-1 px-1 text-sm font-medium">
                     Settings
                   </legend>
                   <div className="grid gap-3">
-                    <Label htmlFor="model">Model</Label>
-                    <Select>
-                      <SelectTrigger
-                        id="model"
-                        className="items-start [&_[data-description]]:hidden"
-                      >
-                        <SelectValue placeholder="Select a model" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="genesis">
-                          <div className="flex items-start gap-3 text-muted-foreground">
-                            <Rabbit className="size-5" />
-                            <div className="grid gap-0.5">
-                              <p>
-                                Neural{" "}
-                                <span className="font-medium text-foreground">
-                                  Genesis
-                                </span>
-                              </p>
-                              <p className="text-xs" data-description>
-                                Our fastest model for general use cases.
-                              </p>
-                            </div>
-                          </div>
-                        </SelectItem>
-                        <SelectItem value="explorer">
-                          <div className="flex items-start gap-3 text-muted-foreground">
-                            <Bird className="size-5" />
-                            <div className="grid gap-0.5">
-                              <p>
-                                Neural{" "}
-                                <span className="font-medium text-foreground">
-                                  Explorer
-                                </span>
-                              </p>
-                              <p className="text-xs" data-description>
-                                Performance and speed for efficiency.
-                              </p>
-                            </div>
-                          </div>
-                        </SelectItem>
-                        <SelectItem value="quantum">
-                          <div className="flex items-start gap-3 text-muted-foreground">
-                            <Turtle className="size-5" />
-                            <div className="grid gap-0.5">
-                              <p>
-                                Neural{" "}
-                                <span className="font-medium text-foreground">
-                                  Quantum
-                                </span>
-                              </p>
-                              <p className="text-xs" data-description>
-                                The most powerful model for complex
-                                computations.
-                              </p>
-                            </div>
-                          </div>
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                  <div className="grid gap-3">
                     <Label htmlFor="temperature">Temperature</Label>
-                    <Input id="temperature" type="number" placeholder="0.4" />
+                    <Input id="temperature" type="float" placeholder="0.4" />
                   </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="top-p">Top P</Label>
-                    <Input id="top-p" type="number" placeholder="0.7" />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="top-k">Top K</Label>
-                    <Input id="top-k" type="number" placeholder="0.0" />
+                  <div className="grid grid-cols-2 gap-4">
+                    <div className="grid gap-3">
+                      <Label htmlFor="top-p">Top P</Label>
+                      <Input id="top_p" type="float" placeholder="0.7" />
+                    </div>
                   </div>
                 </fieldset>
                 <fieldset className="grid gap-6 rounded-lg border p-4">
@@ -177,9 +117,9 @@ export function Dashboard() {
                   </legend>
                   <div className="grid gap-3">
                     <Label htmlFor="role">Role</Label>
-                    <Select defaultValue="system">
+                    <Select name="role" defaultValue="system">
                       <SelectTrigger>
-                        <SelectValue placeholder="Select a role" />
+                        <SelectValue id="role" placeholder="Select a role" />
                       </SelectTrigger>
                       <SelectContent>
                         <SelectItem value="system">System</SelectItem>
@@ -189,167 +129,55 @@ export function Dashboard() {
                     </Select>
                   </div>
                   <div className="grid gap-3">
-                    <Label htmlFor="content">Content</Label>
-                    <Textarea id="content" placeholder="You are a..." />
+                    <Label htmlFor="userContent">Content</Label>
+                    <Textarea
+                      id="roleContent"
+                      placeholder="You are a..."
+                      className="min-h-[9.5rem]"
+                    />
                   </div>
                 </fieldset>
-              </form>
-            </DrawerContent>
-          </Drawer>
-        </header>
-        <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
-          <div
-            className="relative hidden flex-col items-start gap-8 md:flex"
-            x-chunk="dashboard-03-chunk-0"
-          >
-            <form className="grid w-full items-start gap-6">
-              <fieldset className="grid gap-6 rounded-lg border p-4">
-                <legend className="-ml-1 px-1 text-sm font-medium">
-                  Settings
-                </legend>
-                <div className="grid gap-3">
-                  <Label htmlFor="model">Model</Label>
-                  <Select>
-                    <SelectTrigger
-                      id="model"
-                      className="items-start [&_[data-description]]:hidden"
-                    >
-                      <SelectValue placeholder="Select a model" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="genesis">
-                        <div className="flex items-start gap-3 text-muted-foreground">
-                          <Rabbit className="size-5" />
-                          <div className="grid gap-0.5">
-                            <p>
-                              Neural{" "}
-                              <span className="font-medium text-foreground">
-                                Genesis
-                              </span>
-                            </p>
-                            <p className="text-xs" data-description>
-                              Our fastest model for general use cases.
-                            </p>
-                          </div>
-                        </div>
-                      </SelectItem>
-                      <SelectItem value="explorer">
-                        <div className="flex items-start gap-3 text-muted-foreground">
-                          <Bird className="size-5" />
-                          <div className="grid gap-0.5">
-                            <p>
-                              Neural{" "}
-                              <span className="font-medium text-foreground">
-                                Explorer
-                              </span>
-                            </p>
-                            <p className="text-xs" data-description>
-                              Performance and speed for efficiency.
-                            </p>
-                          </div>
-                        </div>
-                      </SelectItem>
-                      <SelectItem value="quantum">
-                        <div className="flex items-start gap-3 text-muted-foreground">
-                          <Turtle className="size-5" />
-                          <div className="grid gap-0.5">
-                            <p>
-                              Neural{" "}
-                              <span className="font-medium text-foreground">
-                                Quantum
-                              </span>
-                            </p>
-                            <p className="text-xs" data-description>
-                              The most powerful model for complex computations.
-                            </p>
-                          </div>
-                        </div>
-                      </SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="temperature">Temperature</Label>
-                  <Input id="temperature" type="number" placeholder="0.4" />
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                  <div className="grid gap-3">
-                    <Label htmlFor="top-p">Top P</Label>
-                    <Input id="top-p" type="number" placeholder="0.7" />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="top-k">Top K</Label>
-                    <Input id="top-k" type="number" placeholder="0.0" />
-                  </div>
-                </div>
-              </fieldset>
-              <fieldset className="grid gap-6 rounded-lg border p-4">
-                <legend className="-ml-1 px-1 text-sm font-medium">
-                  Messages
-                </legend>
-                <div className="grid gap-3">
-                  <Label htmlFor="role">Role</Label>
-                  <Select defaultValue="system">
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select a role" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="system">System</SelectItem>
-                      <SelectItem value="user">User</SelectItem>
-                      <SelectItem value="assistant">Assistant</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="userContent">Content</Label>
-                  <Textarea
-                    id="content"
-                    placeholder="You are a..."
-                    className="min-h-[9.5rem]"
-                  />
-                </div>
-              </fieldset>
-            </form>
-          </div>
-          <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-            <Badge variant="outline" className="absolute right-3 top-3">
-              Output
-            </Badge>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: JSON.stringify(response, null, 2),
-              }}
-            />
-            <div className="flex-1 pt-6">
-              {loading && <p>Loading...</p>}
-              {response && (
-                <p>
-                  <span>{JSON.stringify(response, null, 2)}</span>
-                </p>
-              )}
-              {error && <p>Error: {error.message}</p>}
-            </div>
-            <form
-              className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
-              x-chunk="dashboard-03-chunk-1"
-              onSubmit={handleSubmit}
-            >
-              <Label htmlFor="content" className="sr-only">
-                Message
-              </Label>
-              <Textarea
-                id="content"
-                placeholder="Type your message here..."
-                className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
-              />
-              <div className="flex items-center p-3 pt-0">
-                <Button type="submit" size="sm" className="ml-auto gap-1.5">
-                  Send Message
-                  <CornerDownLeft className="size-3.5" />
-                </Button>
               </div>
-            </form>
-          </div>
+            </div>
+            <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
+              <Badge variant="outline" className="absolute right-3 top-3">
+                Output
+              </Badge>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: JSON.stringify(response, null, 2),
+                }}
+              />
+              <div className="flex-1 pt-6">
+                {loading && <p>Loading...</p>}
+                {response && (
+                  <p>
+                    <span>{JSON.stringify(response, null, 2)}</span>
+                  </p>
+                )}
+                {error && <p>Error: {error.message}</p>}
+              </div>
+              <div
+                className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
+                x-chunk="dashboard-03-chunk-1"
+              >
+                <Label htmlFor="content" className="sr-only">
+                  Message
+                </Label>
+                <Textarea
+                  id="content"
+                  placeholder="Type your message here..."
+                  className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+                />
+                <div className="flex items-center p-3 pt-0">
+                  <Button type="submit" size="sm" className="ml-auto gap-1.5">
+                    Send Message
+                    <CornerDownLeft className="size-3.5" />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </form>
         </main>
       </div>
     </div>
--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -6,16 +6,20 @@ export function useOpenAIChat() {
   const [error, setError] = useState<null | unknown>(null);
   const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
 
-  const fetchChatResponse = async (messages: any) => {
+  const fetchChatResponse = async (messages: any, opts: any) => {
+    console.log("opts", opts);
+    const { temperature, top_p } = opts;
+    console.log("desctruct", temperature, top_p);
     const requestBody = {
       model: "gpt-4-turbo",
       messages: messages,
-      temperature: 1,
+      temperature: temperature ? parseFloat(temperature) : 0,
       max_tokens: 256,
-      top_p: 1,
+      top_p: top_p ? parseFloat(top_p) : 1,
       frequency_penalty: 0,
       presence_penalty: 0,
     };
+    console.log(requestBody);
 
     setLoading(true);
     try {
